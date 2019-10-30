930d1db6cd19acd4c32347c82c419270a1f7269e
Tobin Baker <tobin.d.baker@gmail.com>
2019-10-30 12:42:35 -0700

    Replace unit tests with property tests.

--- src/test/java/set/int32/VerifyHashSet.java
+++ src/test/java/set/int32/VerifyHashSet.java
@@ -1,80 +1,76 @@
 package set.int32;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import hash.int32.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Random;
 import java.util.stream.IntStream;
 
-class VerifyHashSet {
+@RunWith(JUnitQuickcheck.class)
+public class VerifyHashSet {
 
-    private static final int SAMPLE_SIZE = 1 << 10;
-    private static final long SEED = 0xdeadbeefcafebabeL;
-    private static final double[] LOAD_FACTORS = {0.5, 0.75, 0.9, 0.99, 1.0};
+    private static final int SAMPLE_SIZE_LIMIT = 1 << 20;
 
-    private static final Class<?>[] classes = {
-        BLPIntHashSet.class,
-        LCFSIntHashSet.class,
-        LPIntHashSet.class,
-        RHIntHashSet.class,
-    };
+    private static final Class<?>[] classes = { BLPIntHashSet.class, LCFSIntHashSet.class, LPIntHashSet.class,
+            RHIntHashSet.class, };
 
-    @Test
-    void testRandomKeys() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        for (double loadFactor : LOAD_FACTORS) {
-            for (Class<?> cls : classes) {
-                Constructor<?> constructor = cls.getConstructor(int.class, double.class);
-                IntSet set = (IntSet) constructor.newInstance(SAMPLE_SIZE, loadFactor);
-                int[] ints = new Random(SEED).ints(SAMPLE_SIZE).toArray();
-                for (int i : ints) {
-                    // if this ever happens, then we'll just pick a different seed
-                    assert i != 0;
-                }
-                for (int i : ints) {
-                    assertTrue(set.add(i));
-                }
-                assertEquals(SAMPLE_SIZE, set.size());
-                for (int i : ints) {
-                    assertTrue(set.contains(i));
-                }
-                for (int i : ints) {
-                    assertTrue(set.remove(i));
-                }
-                assertEquals(0, set.size());
-                for (int i : ints) {
-                    assertFalse(set.contains(i));
-                }
+    @Property
+    public void testSequentialKeys(@InRange(minInt = 0, maxInt = SAMPLE_SIZE_LIMIT) int sampleSize,
+            @InRange(minDouble = 0.0, maxDouble = 1.0) double loadFactor)
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        for (Class<?> cls : classes) {
+            Constructor<?> constructor = cls.getConstructor(int.class, double.class);
+            IntSet set = (IntSet) constructor.newInstance(sampleSize, loadFactor);
+            int[] ints = IntStream.rangeClosed(1, sampleSize).toArray();
+            for (int i : ints) {
+                assertTrue(set.add(i));
+            }
+            assertEquals(sampleSize, set.size());
+            for (int i : ints) {
+                assertTrue(set.contains(i));
+            }
+            for (int i : ints) {
+                assertTrue(set.remove(i));
+            }
+            assertEquals(0, set.size());
+            for (int i : ints) {
+                assertFalse(set.contains(i));
             }
         }
     }
 
-    @Test
-    void testSequentialKeys() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        for (double loadFactor : LOAD_FACTORS) {
-            for (Class<?> cls : classes) {
-                Constructor<?> constructor = cls.getConstructor(int.class, double.class);
-                IntSet set = (IntSet) constructor.newInstance(SAMPLE_SIZE, loadFactor);
-                int[] ints = IntStream.rangeClosed(1, SAMPLE_SIZE).toArray();
-                for (int i : ints) {
-                    assertTrue(set.add(i));
-                }
-                assertEquals(SAMPLE_SIZE, set.size());
-                for (int i : ints) {
-                    assertTrue(set.contains(i));
-                }
-                for (int i : ints) {
-                    assertTrue(set.remove(i));
-                }
-                assertEquals(0, set.size());
-                for (int i : ints) {
-                    assertFalse(set.contains(i));
-                }
+    @Property
+    public void testRandomKeys(@InRange(minInt = 0, maxInt = SAMPLE_SIZE_LIMIT) int sampleSize,
+            @InRange(minDouble = 0.0, maxDouble = 1.0) double loadFactor)
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        for (Class<?> cls : classes) {
+            Constructor<?> constructor = cls.getConstructor(int.class, double.class);
+            IntSet set = (IntSet) constructor.newInstance(sampleSize, loadFactor);
+            // we want a random permutation, not an RNG, to avoid duplicate keys,
+            // and the Phi hash has quasi-uniform behavior on sequential integers
+            IntHasher hasher = new PhiIntHasher();
+            int[] ints = IntStream.rangeClosed(1, sampleSize).map(hasher::hash).toArray();
+            for (int i : ints) {
+                assertTrue(set.add(i));
+            }
+            assertEquals(sampleSize, set.size());
+            for (int i : ints) {
+                assertTrue(set.contains(i));
+            }
+            for (int i : ints) {
+                assertTrue(set.remove(i));
+            }
+            assertEquals(0, set.size());
+            for (int i : ints) {
+                assertFalse(set.contains(i));
             }
         }
     }
-
 }
